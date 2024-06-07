b55192837092586b8fe333b967e66782338c252e
김윤미 <younmeet@gmail.com>
2024-06-07 20:28:30 +0900

    feat: CountryItem 컴포넌트 구현

---
+++ src/components/CountryItem.jsx
@@ -0,0 +1,41 @@
+import style from './CountryItem.module.css';
+import { Link } from 'react-router-dom';
+
+export default function CountryItem({
+  code,
+  flagImg,
+  commonName,
+  flagEmoji,
+  region,
+  capital,
+  population,
+}) {
+  return (
+    <li className={style['country-item']}>
+      <Link to={`country/${code}`}>
+        <div className={style['card-image']}>
+          <img src={flagImg} alt={commonName} />
+        </div>
+        <div className={style['card-text']}>
+          <h3 className={style['country-name']}>
+            {flagEmoji} {commonName}
+          </h3>
+          <dl className={style['country-info']}>
+            <div className={style['country-info-item']}>
+              <dt>지역:</dt>
+              <dd>{region}</dd>
+            </div>
+            <div className={style['country-info-item']}>
+              <dt>수도:</dt>
+              <dd>{capital}</dd>
+            </div>
+            <div className={style['country-info-item']}>
+              <dt>인구:</dt>
+              <dd>{population}</dd>
+            </div>
+          </dl>
+        </div>
+      </Link>
+    </li>
+  );
+}
