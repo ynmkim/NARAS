4f27192084808c67f4730b54d7adcd66b4c70835
김윤미 <younmeet@gmail.com>
2024-06-07 20:25:38 +0900

    feat: 홈으로 가기 링크 추가 및 레이아웃 스타일 수정

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,13 @@
 import style from './Layout.module.css';
+import { Link } from 'react-router-dom';
 
 export default function Layout({ children }) {
   return (
     <div className="layout">
       <header className={style.header}>
-        <h1 className={style.logo}>NARAS 🌏</h1>
+        <h1 className={style.logo}>
+          <Link to={'/'}> NARAS 🌏</Link>
+        </h1>
       </header>
       <main className={style.main}>{children}</main>
     </div>
